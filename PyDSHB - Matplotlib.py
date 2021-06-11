dee51ad86e54cb34234fa5e4911b0f73fa82a08c
dbgomez94 <dbgomez94@gmail.com>
2021-06-11 13:02:40 -0400

    initial commit

--- PyDSHB - Matplotlib.py
+++ PyDSHB - Matplotlib.py
@@ -1,6 +1,7 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.datasets import load_iris
 
 plt.style.use('classic')
 
@@ -17,21 +18,83 @@ plt.xlabel('x')
 plt.ylabel('y')
 
 # more powerful object oriented-style plotting
-fig, ax = plt.subplots(2)
+f1, ax = plt.subplots(2)
 ax[0].plot(x, np.sin(x))
 ax[1].plot(x, np.cos(x))
 
-fig1 = plt.figure()
+plt.figure()
 ax1 = plt.axes()
 ax1.plot(x, np.sin(x))
 
-fig2 = plt.figure()
+plt.figure()
 ax2 = plt.axes()
 ax2.plot(x, np.cos(x))
 
-# this line is from pycharm
-# this line is also from pycharm
+# scatter plots with plt.scatter
+plt.figure()
+plt.style.use('seaborn-whitegrid')
+rng = np.random.RandomState(0)
+x = rng.randn(100)  # -1 to 1
+y = rng.randn(100)  # -1 to 1
+colors = rng.rand(100)  # 0 to 1
+sizes = 1000 * rng.rand(100)  # 0 to 1 x 1000 so 0 to 1000
 
-"""
-So I'm adding even more here.
-"""
+plt.scatter(x, y, c=colors, s=sizes, alpha=0.3, cmap='viridis')
+plt.colorbar()
+
+iris = load_iris()
+features = iris.data.T
+plt.scatter(features[0], features[1], alpha=0.2, s=100*features[3], c=iris.target, cmap='viridis')
+plt.xlabel(iris.feature_names[0])
+plt.ylabel(iris.feature_names[1])
+
+# error bars
+x = np.linspace(0, 10, 50)
+dy = 0.8
+y = np.sin(x) + dy * np.random.randn(50)
+
+plt.figure()
+plt.errorbar(x, y, yerr=dy, fmt='.k')
+
+plt.figure()
+plt.errorbar(x, y, yerr=dy, fmt='o', color='k', ecolor='lightgray', elinewidth=3, capsize=0)
+
+## Density and contour plots
+
+
+def f(x, y):
+    return np.sin(x) ** 10 + np.cos(10 + y * x) * np.cos(x)
+
+
+x = np.linspace(0, 5, 50)
+y = np.linspace(0, 5, 40)
+
+X, Y = np.meshgrid(x, y)
+Z = f(X, Y)
+
+plt.figure()
+plt.contourf(X, Y, Z, 20, cmap='RdGy')
+plt.colorbar()
+
+plt.figure()
+plt.imshow(Z, extent=[0, 5, 0, 5], origin='lower', cmap='RdGy')
+plt.colorbar()
+
+plt.figure()
+contours = plt.contour(X, Y, Z, 3)
+plt.clabel(contours, inline=True, fontsize=8)
+plt.imshow(Z, extent=[0, 5, 0, 5], origin='lower', cmap='RdGy', alpha=0.5)
+plt.colorbar()
+
+## Histograms, binnings, and density
+data = np.random.randn(1000)
+plt.figure()
+plt.hist(data, bins=100, alpha=0.5, histtype='stepfilled', color='steelblue')
+
+x1 = np.random.normal(1, 1, 1000)  # normal(location, scale, size)
+x2 = np.random.normal(2, 1, 1000)
+x3 = np.random.normal(3, 1, 1000)
+plt.figure()
+plt.hist(x1)
+plt.hist(x2)
+plt.hist(x3)
--- PyDSHB - Numpy.py
+++ PyDSHB - Numpy.py
@@ -1,3 +1,5 @@
 import numpy as np
 
-x = np.arange(10)
+x = np.arange(9)
+x.reshape(3, 3)
+x.reshape((1, 9))
