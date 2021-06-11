06a6a0f83735ef5d29c360d160972eefc005bb0f
dbgomez94 <dbgomez94@gmail.com>
2021-06-11 10:17:41 -0400

    initial commit

---
+++ PyDSHB - Matplotlib.py
@@ -0,0 +1,32 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.style.use('classic')
+
+# %% show() or no show()?
+x = np.linspace(0, 10, 100)
+
+# MATLAB-style plotting
+plt.figure()  # create a plot figure
+plt.subplot(2, 1, 1)   # same as MATLAB (rows, cols, subplot)
+plt.plot(x, np.sin(x))
+plt.subplot(2, 1, 2)
+plt.plot(x, np.cos(x))
+plt.xlabel('x')
+plt.ylabel('y')
+
+# more powerful object oriented-style plotting
+fig, ax = plt.subplots(2)
+ax[0].plot(x, np.sin(x))
+ax[1].plot(x, np.cos(x))
+
+fig1 = plt.figure()
+ax1 = plt.axes()
+ax1.plot(x, np.sin(x))
+
+fig2 = plt.figure()
+ax2 = plt.axes()
+ax2.plot(x, np.cos(x))
+
+# this line is from pycharm
---
+++ PyDSHB - Pandas.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+
+# %% The Panda Series Object
+data = pd.Series(np.linspace(0.25, 1, 4), index=list('ABCD')) # series
+data_values = data.values     # attribute, returns: ndarray
+data_index = data.index      # attribute, returns: RangeIndex
+
+mydict = {'A': 0, 'B': 1, 'C': 2, 'D': 3}  # dynamic type indices and values
+seriesfromdict = pd.Series(mydict)  # static type indices and values
+
+# %% The Pandas DataFrame Object
+s1 = pd.Series(np.random.randint(0, 100, 4), index=list('ABCD'))
+s2 = pd.Series(np.random.randint(0, 100, 4), index=list('ABCD'))
+s3 = pd.Series(np.random.randint(0, 100, 4), index=list('ABCD'))
+df = pd.DataFrame({'Series 1': s1, 'Series 2': s2, 'Series 3': s3})
+
+# % The Pandas Index Object
+ind = pd.Index([2, 3, 5, 7, 11])
+
+# %% Operating and Data in Pandas
+
