965ec620abbcb127a83b51fee1f056de8aca4dd7
dbgomez94 <dbgomez94@gmail.com>
2021-06-12 10:37:15 -0400

    Initial commit

--- PyDSHB - Numpy.py
+++ PyDSHB - Numpy.py
@@ -1,8 +1,12 @@
+# %%
 import numpy as np
 
+# %%
 x = np.arange(9)
 x.reshape(3, 3)
+# %%
 x.reshape((1, 9))
+# %%
 
-# union of two vectors
+# %% union of two vectors
 np.union1d([1, 2, 3], [3, 4, 5])
