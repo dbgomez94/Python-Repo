e5de504d96f530236a124ddc3a3a8b3464d7fe81
dbgomez94 <dbgomez94@gmail.com>
2021-06-15 17:57:22 -0400

    Updated PyDSHB - Pandas.py.

--- PyDSHB - Pandas.py
+++ PyDSHB - Pandas.py
@@ -16,27 +16,25 @@ pop['CA'] # etc
 # Advantage of series is array-style indexing (e.g., splicing)
 # pop_dict['CA':'NY'] # returns error
 pop['CA':'NY'] # works
-
 """
 Several ways to create a series from scratch
     pd.Series(data, index=index)
     Data = list, np array, scalar, dictionary (shown above)
     index = auto starting from 0 if undefined, typically an array like entity
 """
-
-mydict = {'A': 0, 'B': 1, 'C': 2, 'D': 3}  # dynamic type indices and values
+mydict = {'R0': 0, 'R1': 1, 'R2': 2, 'R3': 3}  # dynamic type indices and values
 myseries = pd.Series(mydict)  # static type indices and values
 
-# %% The Pandas DataFrame Object
 
+# %% The Pandas DataFrame Object
 " can be built from multiple series with the same index object "
-ind = list('ABCD')
+ind = ['R0', 'R1', 'R2', 'R3']
+s0 = pd.Series(np.random.randint(0, 100, 4), index=ind)
 s1 = pd.Series(np.random.randint(0, 100, 4), index=ind)
 s2 = pd.Series(np.random.randint(0, 100, 4), index=ind)
-s3 = pd.Series(np.random.randint(0, 100, 4), index=ind)
 # each series here shares the same index ind
 # we'll build a dict from these with unique keys, and turn the dict into a df. The keys will default to column names
-df = pd.DataFrame({'S1': s1, 'S2': s2, 'S3': s3})
+df = pd.DataFrame({'C0': s0, 'C1': s1, 'C2': s2})
 # index and column attributes are both index objects!
 df.index
 df.columns
@@ -45,44 +43,51 @@ there are many ways to construct a dataframe
 pd.DataFrame(data, columns=col)
 data = list of dictionaries, dictionary of series objects, 2d np array, np structured array
 """
-
 # % The Pandas Index Object
 ind = pd.Index([2, 3, 5, 7, 11]) # index object is an immutable array / ordered set
+
 
 # %% Data Selection in DataFrames
 """ 
 recall a df is like a dictionary of series, all sharing the same index, with column names serving as the "keys".
 individual series are accessed via dictionary-style item access:
 """
-df['S1']
+df['C1']
 """
-if col is nice string, can also use df.S1 (attribute style), doesn't work all the time, 
+if col is nice string, can also use df.C1 (attribute style), doesn't work all the time, 
 can be confused with other attributes, not recommended
 """
 # dict-style item access can also be used to modify the df object (e.g., create new column)
-df['S4'] = df['S1'] + df['S2'] + df['S3']
+df['C3'] = df['C0'] + df['C1'] + df['C2']
 """
 majority of indexing dfs will be in this column-wise fashion, but suppose you're interested in accessing row items?
 (what if row index is same as column index?)
 this is generally referred to as "array-style item access"
 use iloc, loc, and ix to index as an array
+
+two kinds:
+1. implicit - uses implicit underlying indices (always 0 to n-1), but row and col names are preserved!
+2. explicit - uses the explicitly designated indices (can be strings, numbers, etc.)
 """
+ri = 2
+ci = 3
+rx = 'R2'
+cx = 'C3'
+df.iloc[ri]  # returns the row at index [r] as a pd ** Series ** with the column names now the index object
+df.loc[rx]
 
+df.iloc[:, ci]  # returns all the rows and the column at c
+df.loc[:, cx]
 
+df.iloc[ri,  ci]  # returns element at index [r, c], just like a 2d array
+df.loc[rx, cx]
 
+df.iloc[:ri, :ci]  # all up to but not including r, and all up to but not including c
+df.loc[:rx, :cx]  # all up to AND including r, and all up to AND including c
+"""
+WARNING on splicing: explicit splicing is INCLUSIVE (whereas implicit splicing is exclusive)
+"""
 
 # %% Operating and Data in Pandas
 # Ufuncs: Index preservation
-ser = pd.Series(np.random.randint(0, 10, 4))
-df1 = pd.DataFrame(np.random.randint(0, 10, (3, 4)), columns=['c0', 'c1', 'c2', 'c3'], index=['r0', 'r1', 'r2'])
-# Ufuncs: Index alignment
-s3['E'] = np.random.randint(100)
-s4 = s1 / s3  # resulting array contains the union of the indices of s1 and s3
-s1.index.union(s3.index)
-
-df2 = df1 * 2
-
-df1['c0']['r0'] = np.nan
-
-df1.dropna(axis='columns')
 
